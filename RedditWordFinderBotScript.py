9bc7ec4639f8afd7a4eaf4a41f7c6ccc2855b60a
ZeeeSpy <ZeeeSpy@gmail.com>
2019-11-23 15:35:08 +0000

    Start work on bot script

--- RedditWordFinderBotScript.py
+++ RedditWordFinderBotScript.py
@@ -6,6 +6,7 @@ import string
 from dotenv import load_dotenv
 import os
 import WordMapper
+import time
 #import good stuff
 
 
@@ -15,12 +16,12 @@ load_dotenv(os.path.join(BASEDIR,".env"))
 
 reddit = praw.Reddit(client_id=os.getenv("ID"),
                      client_secret=os.getenv("SECRET"),
-                     user_agent=os.getenv("AGENT"))
+                     user_agent=os.getenv("AGENT"),
+                     username=os.getenv("THISUSER"),
+                     password=os.getenv("PASS"))                        
 #set up API
-#For some reason dotenv doesn't like hyphens in .env values 
-print("Connected To Reddit:",reddit.read_only)
-#check if in read only 
 
+"""
 #update wordmap
 textinput = input("Update WordMap? Y/N:")
 if (textinput == "Y" or textinput == "y"):
@@ -28,23 +29,17 @@ if (textinput == "Y" or textinput == "y"):
     print("New Words Mapped")
 else:
     print("Old Map Used")
-
+"""
 #load wordmap
 WordsToFind = np.load('WordMap.npy',allow_pickle='TRUE').item()
 
-textinput = input("Redditor to check: \n")
-redditor = reddit.redditor(textinput)
-counter = 0
-foundsomething = 0
-
-try:
-    if redditor.id:
-        print("User Is Valid")    
-except:
-    print(textinput +" not found")
-    exit()
-#Check if user is valid
-
+while True:
+    for mention in reddit.inbox.mentions():
+        print (mention)
+    print("Check Done")
+    time.sleep(5) #so the api doesn't get mad
+    
+"""
 for comment in redditor.comments.new(limit=None):
     counter +=1
     print (str(counter)+"/1000")
@@ -62,3 +57,4 @@ if (foundsomething == 1):
         print (i ,":", WordsToFind[i])
 else: 
     print (textinput+" is clean")
+"""
